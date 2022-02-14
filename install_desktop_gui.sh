b7f1240e0e50aec1c89841438db0dd33431e2380
sumukshashidhar <sumukuuu@gmail.com>
2022-02-14 03:33:43 -0600

    feat: add gui installation

---
+++ install_desktop_gui.sh
@@ -0,0 +1,6 @@
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install tasksel
+sudo apt-get install slim
+sudo apt-get install lightdm
+sudo apt-get install xfce4 slim
+sudo service slim start
